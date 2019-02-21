c394d35ecd46546a5a983f7634bfc7d0cf3b8438
饭饭 <fanfan@huanhuan.local>
2019-02-21 17:56:26 +0800

    首页增加图表

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="echart" ref="chart" class="echart" />
+  <div>
+    <div class="container">
+      <div id="echart" ref="chart" class="echart1" />
+      <div id="echart2" class="echart2" />
+      <div class="clear" />
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -51,6 +57,78 @@ export default {
         ]
       }
       return obj
+    },
+    option() {
+      const option = {
+        title: {
+          text: '浏览器占比变化',
+          subtext: '纯属虚构',
+          top: 10,
+          left: 10
+        },
+        tooltip: {
+          trigger: 'item',
+          backgroundColor: 'rgba(0,0,250,0.2)'
+        },
+        legend: {
+          type: 'scroll',
+          bottom: 10,
+          data: (function() {
+            var list = []
+            for (var i = 1; i <= 28; i++) {
+              list.push(i + 2000 + '')
+            }
+            return list
+          })()
+        },
+        visualMap: {
+          top: 'middle',
+          right: 10,
+          color: ['red', 'yellow'],
+          calculable: true
+        },
+        radar: {
+          indicator: [
+            { text: 'IE8-', max: 400 },
+            { text: 'IE9+', max: 400 },
+            { text: 'Safari', max: 400 },
+            { text: 'Firefox', max: 400 },
+            { text: 'Chrome', max: 400 }
+          ]
+        },
+        series: (function() {
+          var series = []
+          for (var i = 1; i <= 28; i++) {
+            series.push({
+              name: '浏览器（数据纯属虚构）',
+              type: 'radar',
+              symbol: 'none',
+              lineStyle: {
+                width: 1
+              },
+              emphasis: {
+                areaStyle: {
+                  color: 'rgba(0,250,0,0.3)'
+                }
+              },
+              data: [
+                {
+                  value: [
+                    (40 - i) * 10,
+                    (38 - i) * 4 + 60,
+                    i * 5 + 10,
+                    i * 9,
+                    i * i / 2
+                  ],
+                  name: i + 2000 + ''
+                }
+              ]
+            })
+          }
+          return series
+        })()
+      }
+      return option
     }
   },
   mounted() {
@@ -59,15 +137,25 @@ export default {
   methods: {
     setChats() {
       const myChart = this.$echarts.init(document.getElementById('echart'))
+      const myChart2 = this.$echarts.init(document.getElementById('echart2'))
       myChart.setOption(this.opt)
+      myChart2.setOption(this.option)
     }
   }
 }
 </script>
 <style>
-.echart {
+.container {
+  width: 100%;
+}
+.clear {
+  clear: both;
+}
+.echart1,
+.echart2 {
   width: 500px;
   height: 400px;
-  margin: 30px auto;
+  margin: 50px;
+  float: left;
 }
 </style>
